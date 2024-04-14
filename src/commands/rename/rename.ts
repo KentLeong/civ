ea74633f112b78c2117e818ca070ea56122d4596
Leong <Erickentleongchu@gmail.com>
2024-04-14 02:21:29 -0400

    feat(rename): finished

--- src/commands/rename/rename.ts
+++ src/commands/rename/rename.ts
@@ -5,11 +5,33 @@ import { perm } from "../../lib/perm";
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("rename")
-    .setDescription("renames user"),
+    .setDescription("renames user")
+    .addUserOption(option =>
+      option.setName("user")
+        .setDescription("The user to rename.")
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName("name")
+        .setDescription("The new name of the user.")
+        .setRequired(true)),
   async execute(interaction: ChatInputCommandInteraction) {
     if (perm(interaction, "leong") == false) {
       await interaction.reply("You do not have permission to use this command.");
       return;
     }
+    const user = interaction.options.getUser("user");
+    const name = interaction.options.getString("name");
+
+    const exists = await User.findOne({
+      discordId: user?.id
+    });
+
+    if (!exists) {
+      await interaction.reply("User not found.");
+      return;
+    }
+
+    await User.updateOne({ discordId: user?.id }, { name: name });
+    await interaction.reply("User renamed.");
   },
 }
