b6b22eccc9aa7c99a7069b48ae017ad5da3011ea
Leong <Erickentleongchu@gmail.com>
2024-04-14 03:27:40 -0400

    feat(host): displays lobby

--- src/commands/host/host.ts
+++ src/commands/host/host.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
-import { Game } from "../../mongo"
-import { perm } from "../../lib/perm";
+import { Game, User } from "../../mongo"
+import { perm, lobby } from "../../lib";
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,10 +15,19 @@ module.exports = {
     // checks if user is already hosting a game
     const exists = await Game.findOne({
       host: interaction.user.id,
-      $or: [{ status: "lobby" }, { status: "started" }, {status: "draft"}]
+      $or: [{ state: "lobby" }, { state: "started" }, {state: "draft"}]
     });
     if (exists) {
       await interaction.reply("You are already hosting a game.");
+      return;
+    }
+
+    // find user
+    const user = await User.findOne({
+      discordId: interaction.user.id
+    });
+    if (!user) {
+      await interaction.reply("User not found.");
       return;
     }
 
@@ -27,7 +36,29 @@ module.exports = {
     const newGame = await Game.create({
       id: gameCount,
       host: interaction.user.id,
-      state: "lobby"
+      state: "lobby",
+      players: [
+        {
+          discordId: interaction.user.id,
+          name: user.name,
+          role: "host",
+          bans: [],
+          civ: "",
+          team: 1,
+          pool: []
+        }
+      ],
+      settings: {
+        bans: 3,
+        mode: "standard",
+        ranked: true,
+        modVer: "LekMod_v32.2",
+        map: "LekMap_v5.1"
+      }
     });
+    await newGame.save();
+
+    let display = await lobby(newGame);
+    await interaction.channel?.send({ embeds: [display]});
   },
 }
---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+import { perm } from "./perm";
+import { lobby } from "./lobby";
+
+export { perm, lobby };
