1e09fd4a8972cd41e10b14bd5afb490106d94527
Leong <Erickentleongchu@gmail.com>
2024-04-14 02:15:27 -0400

    feat(perm): added perm lib

---
+++ src/lib/perm.ts
@@ -0,0 +1,10 @@
+import { ChatInputCommandInteraction } from "discord.js";
+
+export const perm = (interaction: ChatInputCommandInteraction, type: string): boolean => {
+  if (type == "leong" && interaction.user.id !== "752025586171510865") {
+    return false;
+  } else if (type == "mod" && interaction.user.id !== "752025586171510865") {
+    return false;
+  }
+  return true;
+}
