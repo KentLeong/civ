90da7c93a0e69fa187efc3139714fab66a7ffbb6
Leong <Erickentleongchu@gmail.com>
2024-04-14 02:07:54 -0400

    feat(signup): finished

--- src/commands/signup/signup.ts
+++ src/commands/signup/signup.ts
@@ -1,10 +1,40 @@
-import { SlashCommandBuilder, CommandInteraction } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { User } from "../../mongo";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("signup")
-    .setDescription("User signup command."),
-  async execute(interaction: CommandInteraction) {
-    
+    .setDescription("User signup command.")
+    .addUserOption(option =>
+      option.setName("user")
+        .setDescription("The user to signup.")
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName("name")
+        .setDescription("The name of the user.")
+        .setRequired(true)),
+  async execute(interaction: ChatInputCommandInteraction) {
+    if (interaction.user.id !== "752025586171510865") {
+      await interaction.reply("Please ask Leong to sign you up.");
+      return;
+    }
+    const user = interaction.options.getUser("user");
+    const name = interaction.options.getString("name");
+
+    const exists = await User.findOne({
+      discordId: user?.id
+    });
+
+    if (exists) {
+      await interaction.reply("User already signed up.");
+      return;
+    }
+
+    const newUser = await User.create({
+      name: name,
+      discordId: user?.id
+    });
+    await newUser.save();
+    await interaction.reply("User signed up.");
   },
 }
--- src/mongo.ts
+++ src/mongo.ts
@@ -1,18 +1,26 @@
 import mongoose from "mongoose";
 import { CivSchema } from "./models/civ";
-import { Civilization } from "./types";
+import { UserSchema } from "./models/user";
+import { GameSchema } from "./models/game";
+import { Civilization, User, Game } from "./types";
 
 var mongo: mongoose.Connection;
-var Civ: mongoose.Model<Civilization>
+var Civ: mongoose.Model<Civilization>;
+var User: mongoose.Model<User>;
+var Game: mongoose.Model<Game>;
 
 try {
   mongo = mongoose.createConnection(process.env.MONGO_CRED || "").useDb("civ");
   Civ = mongo?.model("Civilizations", CivSchema);
+  User = mongo?.model("Users", UserSchema);
+  Game = mongo?.model("Games", GameSchema);
 } catch (error) {
   console.error(error);
 }
 
 export {
   mongo,
-  Civ
+  Civ,
+  User,
+  Game
 }
