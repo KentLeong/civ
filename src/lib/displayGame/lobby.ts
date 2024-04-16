5b97771f3bdef08515afb44c5028c7f01214470e
Leong <Erickentleongchu@gmail.com>
2024-04-15 22:29:17 -0400

    refactor: added space before bans

--- src/lib/displayGame/lobby.ts
+++ src/lib/displayGame/lobby.ts
@@ -10,9 +10,9 @@ export default async (interaction: any, game: Game) => {
   game.players.forEach((player, i) => {
     description += "```bash\n"+(i+1)+". "+player.name;
     if (player.bans.length == 0) {
-      description += "\n# Bans: None```";
+      description += "\n#  Bans: None```";
     } else {
-      description += "\n# Bans: "+player.bans.join(", ")+"```";
+      description += "\n#  Bans: "+player.bans.join(", ")+"```";
     }
   });
   description += "\nYou can have up to two bans using the `/ban` command."
