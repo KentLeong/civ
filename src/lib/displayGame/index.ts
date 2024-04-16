2755a8bcd8f5cf0e81556013555aaca626bb3373
Leong <Erickentleongchu@gmail.com>
2024-04-15 22:44:38 -0400

    refactor(ingame): reformated the displays

--- src/lib/displayGame/index.ts
+++ src/lib/displayGame/index.ts
@@ -2,11 +2,13 @@ import { Game } from "../../mongo";
 import init  from './init';
 import lobby from "./lobby";
 import draft from "./draft";
+import ingame from "./ingame";
 
 const states:Record<string, any> = {
   "init": init,
   "lobby": lobby,
   "draft": draft,
+  "ingame": ingame,
 }
 
 export default async (interaction:any, game: Game) => {
--- src/lib/displayGame/ingame.ts
+++ src/lib/displayGame/ingame.ts
@@ -1,63 +1,58 @@
-import { AttachmentBuilder, EmbedBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder } from "discord.js";
+import { EmbedBuilder, ButtonStyle, ButtonBuilder, ActionRowBuilder } from "discord.js";
 import { Game } from "../../mongo";
 import { expireReply } from "../../lib";
 import { LobbyEvent, GameEvent, Player } from "../../types";
 
 export default async (interaction:any, game: Game) => {
-  let description = "```fix\n"+" ".repeat(15)+"Civ 5 - In Game "+" ".repeat(15)+"```\n";
+  let description = "```fix\n"+" ".repeat(20)+"Civ 5 - In Game "+" ".repeat(20)+"```\n";
   let playerField = "";
   let GameDetailsField = "";
-  const file = new AttachmentBuilder("src/assets/civ5.png");
   const embed = new EmbedBuilder()
-    .setThumbnail("attachment://civ5.png")
-  
-  const cancel = new ButtonBuilder()
-    .setCustomId("cancelGame")
-    .setLabel("Cancel Game")
-    .setStyle(ButtonStyle.Danger);
 
   const done = new ButtonBuilder()
     .setCustomId("doneGame")
     .setLabel("Done")
     .setStyle(ButtonStyle.Success);
   const row: any = new ActionRowBuilder()
-    .addComponents(cancel, done);
+    .addComponents(done);
+
+  // lists lobby events
+  if (game.lobbyEvents.length > 0) {
+    description += "```bash\n";
+    game.lobbyEvents.forEach((event: LobbyEvent) => {
+      if (event.type == "select") {
+        description += "\n#  "+event.players[0].name+" selected "+event.civ;
+      } else if (event.type == "random") {
+        description += "\n#  "+event.players[0].name+" randomed "+event.civ;
+      } else if (event.type == "trade") {
+        description += "\n#  "+event.players[0].name+" traded with "+event.players[1].name;
+      }
+    });
+    description += "```";
+  }
 
   // shows how long the game has been going on
   GameDetailsField += "```bash\n";
   if (!game.startedAt) {
-    GameDetailsField += "Game Duration: Just started";
+    GameDetailsField += "Time: Just started";
   } else {
     let time = new Date().getTime() - game.startedAt.getTime();
     let hours = Math.floor(time / 3600000);
     let minutes = Math.floor((time % 3600000) / 60000);
-    GameDetailsField += "Game Duration: "+hours+"h "+minutes+"m";
+    GameDetailsField += "Time: "+hours+"h "+minutes+"m";
   }
   GameDetailsField += "```";
   
-  
-  // lists lobby events
-  if (game.lobbyEvents.length > 0) {
-    GameDetailsField += "```bash\n";
-    game.lobbyEvents.forEach((event: LobbyEvent) => {
-      if (event.type == "select") {
-        GameDetailsField += "\n#  "+event.players[0].name+" selected "+event.civ;
-      } else if (event.type == "random") {
-        GameDetailsField += "\n#  "+event.players[0].name+" randomed "+event.civ;
-      } else if (event.type == "trade") {
-        GameDetailsField += "\n#  "+event.players[0].name+" traded with "+event.players[1].name;
-      }
-    });
-    GameDetailsField += "```";
-  }
-  
   // lists Game events
+  GameDetailsField += "\n**Game Events:**"
   if (game.gameEvents.length > 0) {
     GameDetailsField += "```bash\n";
     game.gameEvents.forEach((event: GameEvent) => {
       // do stuff
     });
     GameDetailsField += "```";
+  } else {
+    GameDetailsField += "```bash\n None```";
   }
 
   // moves players to dead or alive list
@@ -79,15 +74,18 @@ export default async (interaction:any, game: Game) => {
   playerField += "\n";
 
   // lists players that are dead
-  deadPlayers.forEach((player, i) => {
-    playerField += "```bash\n"+(i+1)+". "+player.name+" - "+player.civ+"\n#  ";
-    playerField += player.pool.join(", ")+"```";
-  });
+  if (deadPlayers.length > 0) {
+    playerField += "**Dead Players:**\n";
+    deadPlayers.forEach((player, i) => {
+      playerField += "```bash\n"+(i+1)+". "+player.name+" - "+player.civ+"\n#  ";
+      playerField += player.pool.join(", ")+"```";
+    });
+  }
 
   embed.setDescription(description);
   embed.addFields([
-    { name: "Game Details", value: GameDetailsField, inline: true},
-    { name: "Players", value: playerField, inline: true}
+    { name: "Game Details:", value: GameDetailsField, inline: true},
+    { name: "Players:", value: playerField, inline: true}
   ]);
   const message = await interaction.client.channels.cache.get(process.env.GAME_CHANNEL_ID || "")?.messages.fetch(game.messageId);
   if (!message) {
@@ -95,5 +93,5 @@ export default async (interaction:any, game: Game) => {
     expireReply(interaction);
     return;
   }
-  await message.edit({ embeds: [embed], files: [file], components: [row]});
+  await message.edit({ embeds: [embed], components: [row], files: []});
 }
